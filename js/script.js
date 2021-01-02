7dae239e6d9c5b7626c071c257429312164e1d23
NobiusLee <36500893+NobiusLee@users.noreply.github.com>
2021-01-02 20:06:04 +0300

    First commit for Lemon site

---
+++ js/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    $('.burger-menu').click(function (event) {
+        $('.burger-menu, .menu').toggleClass('active');
+        $('body').toggleClass('lock');
+    });
+});
